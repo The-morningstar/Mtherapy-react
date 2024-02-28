d7efd8f1eab80d56d7b02e49f0c575f49994fcb7
The-morningstar <99304394+The-morningstar@users.noreply.github.com>
2024-02-28 12:05:20 +0530

    patch0.1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,16 +6,18 @@ const Footer = () => {
     <div className="footer-container">
       <div className="footer-column">
         <img className='shanthilogo' src={shanthilogo}/>
-        <p className='sdesc'>Palliative care aims to improve the quality of life of people with life-limiting or disabling diseases, by treating pain and by providing emotional, mental and social support. Everyone can be a part of it.</p>
+        <p className='sdesc'>Palliative care aims to improve the quality of life of people <br/>with life-limiting or disabling diseases, by treating pain and by providing <br/>emotional, mental and social support. Everyone can be a part of it.</p>
       </div>
-      <div className="footer-column">
-        <h3>Column 2</h3>
-        <p>Content for the second column goes here.</p>
-      </div>
-      <div className="footer-column">
-        <h3>Column 3</h3>
-        <p>Content for the third column goes here.</p>
-      </div>
+      <div className='footer-column'>
+  <div className="footercontactus">
+    <h3>Contact us</h3>
+    
+    <a  onClick={() => window.location.href = 'tel:+919446997887'}>Anoysious Joju</a>
+    <a  onClick={() => window.location.href = 'tel:+919446997887'}>Nimble nixan</a>
+    <p>shanthibavan@gmail.com</p>
+  </div>
+</div>
+      
       
     </div>
     <div className='rights'><p>&copy; All rights reserved Mobilixion Technologies Pvt. Ltd.</p></div></footer>
